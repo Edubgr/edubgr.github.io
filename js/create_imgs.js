b82f6c44963b3a309e4be1c705278c7a5a1cfc05
Eduardo <eduguibar@gmail.com>
2020-11-13 09:48:57 -0300

    teste1

---
+++ js/create_imgs.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function create_itens(turma,TURMA,cont,total){
+        for(var i=1;i<=total;i++){
+            tem=i+cont
+            var stream = document.querySelector('.gallery__stream');
+            var element = document.createElement("div");
+            element.setAttribute("class","gallery__item "+TURMA)
+            element.setAttribute("id",tem)
+            stream.appendChild(element);
+
+            var div= document.getElementById(tem)
+            var element = document.createElement("div");
+            element.setAttribute("class","bg")
+            element.setAttribute("style","background-image: url"+"(/img/"+turma+"/"+i+".jpg)");
+            var img = new Image();
+            img.src = '/img/'+turma+"/"+i+".jpeg";
+            
+            console.log(img.height,img.width)
+            div.appendChild(element);
+
+            var element = document.createElement("div");
+            element.setAttribute("class","item__name")
+            element.setAttribute("id",turma+tem)
+            div.appendChild(element);
+
+            var div = document.getElementById(turma+tem);
+            var h1 = document.createElement("h1");
+            var text = document.createTextNode("Eduardo");
+            h1.appendChild(text);
+            var h2 = document.createElement("h2");
+            var text = document.createTextNode("Autoria própria");
+            h2.appendChild(text);
+
+            div.appendChild(h1);
+            div.appendChild(h2);
+        }
+    }
+    var num_a=5;
+    var num_b=5;
+    var num_c=5;
+    var num_d=5;
+
+    create_itens("a","A",0,num_a)
+    create_itens("b","B",num_a,num_b)
+    create_itens("c","C",num_a+num_b,num_c)
+    create_itens("d","D",num_a+num_b+num_c,num_d)
+  });
